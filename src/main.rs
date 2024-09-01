93ed899cb7ea47013cf31ba384f880f9854275f6
idkshite <85193567+idkshite@users.noreply.github.com>
2024-09-01 14:37:09 +0000

    filtering out errors

--- src/main.rs
+++ src/main.rs
@@ -9,24 +9,26 @@ fn process_string(input: &str) -> Result<String, String> {
 
 fn main() -> Result<(), String> {
     let dem_strings = vec!["banana", "cat", "robert", "joy"];
-    // println!("{:?}", dem_strings);
 
-    for one_of_dem_strings in dem_strings {
-        let result = process_string(one_of_dem_strings);
+    let results: Vec<_> = dem_strings
+        .into_iter()
+        .filter_map(|one_of_dem_strings| {
+            let result = process_string(one_of_dem_strings);
 
-        // Using map to transform the Ok value
-        let modified = result
-            .map(|s| {
-                if s == "ROBERT" {
-                    return format!("the magnificent {}", s);
-                }
-                s
-            })
-            .map(|s| format!("The {} Characters of: {}", s.chars().count(), s))
-            .map(|s| format!("✅: {}", s));
+            result
+                .map(|s| {
+                    if s == "ROBERT" {
+                        return format!("the magnificent {}", s);
+                    }
+                    s
+                })
+                .map(|s| format!("The {} Characters of: {}", s.chars().count(), s))
+                .map(|s| format!("✅: {}", s))
+                .ok()
+        })
+        .collect();
 
-        println!("{:?}", modified);
-    }
+    results.iter().for_each(|s| println!("{:?}", s));
 
     Ok(())
 }
